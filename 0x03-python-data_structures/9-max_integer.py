43eb048e5dfeb4158027fccd3ca1ae9517b3ef29
Masonga222 <masongashemap@gmail.com>
2023-11-05 01:51:03 -0800

    python-data_structures

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    max_value = 0
+
+    if len(my_list) > 0:
+        for i in my_list:
+            if i > max_value:
+                max_value = i
+    else:
+        return None
+    return max_value

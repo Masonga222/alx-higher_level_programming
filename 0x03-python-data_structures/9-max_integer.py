27e7b02a19bb38c69cdb703127fbd231ad333cc2
Masonga222 <masongashemap@gmail.com>
2023-11-05 03:31:25 -0800

    python-data_structures

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-
-
 def max_integer(my_list=[]):
-    max_value = 0
-
-    if len(my_list) > 0:
-        for i in my_list:
-            if i > max_value:
-                max_value = i
-    else:
-        return None
-    return max_value
+        return (min(my_list, key=lambda i: -i)) if my_list else None

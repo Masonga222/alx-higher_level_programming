a736d9c9dda83be06a84ee576fdef49a1afdb547
Masonga222 <masongashemap@gmail.com>
2023-12-05 11:13:50 -0800

    python-input_output

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+eturns a list of lists of integers
+representing the Pascal’s triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """
+    eturns a list of lists of integers
+    representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+    res = []
+    t = []
+    for x in range(n):
+        row = []
+        for y in range(x + 1):
+            if x == 0 or y == 0 or x == y:
+                row.append(1)
+            else:
+                row.append(t[y] + t[y - 1])
+        t = row
+        res.append(row)
+    return res
